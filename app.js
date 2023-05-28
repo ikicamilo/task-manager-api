5bd33376dfb2d3dd6f62f64779058aceaa504566
ikicamilo <ikicamilo@gmail.com>
2023-05-28 10:03:53 +0100

    Cors added

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-require('./db/connect')
-require('dotenv').config()
-const express = require('express');
+require("./db/connect");
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-const path = require('path')
-const notFound = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/error-handler')
+const cors = require("cors");
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const path = require("path");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static(path.resolve(__dirname, './public')))
-app.use(express.json())
-
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.json());
+app.use(cors());
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-})
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
 
-app.use('/api/v1/tasks', tasks)
-app.use(notFound)
-app.use(errorHandlerMiddleware)
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGODB_URI_03)
-        app.listen(port, console.log(`Server is listenin on port ${port}`))        
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGODB_URI_03);
+    app.listen(port, console.log(`Server is listenin on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
